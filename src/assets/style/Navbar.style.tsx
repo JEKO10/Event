eac7f4c9b50088d9109621275af6da9b42b7f359
Aleksa <aleksa3b@gmail.com>
2023-09-02 21:23:20 +0200

    navbar redesign

--- src/assets/style/Navbar.style.tsx
+++ src/assets/style/Navbar.style.tsx
@@ -1,17 +1,43 @@
-import { styled } from "styled-components";
+import { keyframes, styled } from "styled-components";
 import { flexMixin, secondaryColor } from "./GlobalStyles";
 
+const changeColor = keyframes`
+  0% {
+    color: #fff;
+  }
+  100% {
+    color: ${secondaryColor};
+  }
+`;
+
 export const Nav = styled.nav`
-  padding: 70px 20px;
+  /* padding: 70px 20px; */
+  ${flexMixin({ justify: "space-between", align: "center" })}
+  padding: 30px 40px 50px;
+
+  img {
+    height: 70px;
+    width: auto;
+  }
+
+  div {
+    p {
+      color: ${secondaryColor};
+      font-size: 1.5rem;
+      text-align: center;
+      animation: ${changeColor} 500ms linear infinite alternate;
+    }
+  }
 
   ul {
-    ${flexMixin({ justify: "space-around", align: "center" })};
+    ${flexMixin({ justify: "space-between", align: "center" })};
     color: #fff;
     list-style-type: none;
 
     li {
-      font-size: 2rem;
+      font-size: 1.8rem;
       text-transform: uppercase;
+      margin: 0 2rem;
       cursor: pointer;
       transition: all 200ms ease;
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,10 @@
 import { Nav } from "../assets/style/Navbar.style";
+import Logo from "../assets/images/logo.png";
 
 const Navbar = () => {
   return (
     <Nav>
-      <ul>
+      {/* <ul>
         <li>
           <a>O Beranama</a>
         </li>
@@ -16,19 +17,36 @@ const Navbar = () => {
         {/* <li>
           <a>Odbori</a>
         </li> */}
-        {/* <li>
+      {/* <li>
           <a>Sponzori</a>
         </li> */}
-        <li>
-          <a>Raspored sekcija</a>
-        </li>
-        {/* <li>
+      {/* <li>
+        <a>Raspored sekcija</a>
+      </li> */}
+      {/* <li>
           <a>Foto galerija</a>
-        </li> */}
+        </li> 
         <li>
           <a>Program konferencije</a>
         </li>
+      </ul> */}
+      <img src={Logo} alt="Logo" />
+      <ul>
+        <li>
+          <a>Program konferencije</a>
+        </li>
+        <li>
+          <a>Teme konferencije</a>
+        </li>
+        <li>
+          <a>Cilj konferencije</a>
+        </li>
       </ul>
+      <div>
+        <p>8.9.2023.</p>
+        <p>9.9.2023.</p>
+        <p>10.9.2023.</p>
+      </div>
     </Nav>
   );
 };
