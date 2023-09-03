f1775e472f31852ed703e89a10584bec8d68235e
Aleksa <aleksa3b@gmail.com>
2023-09-03 11:35:04 +0200

    scroll to top

--- src/router/RouterProvider.tsx
+++ src/router/RouterProvider.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import routes from "./routes";
 import Footer from "../components/Footer";
+import ScrollToTop from "./ScrollTop";
 // import Socials from "../components/Socials";
 
 function RouterProvider() {
   return (
     <Router>
       <>
+        <ScrollToTop />
         <Routes>
           {routes.map(({ id, path, element: Component }) => (
             <Route key={id} path={path} element={<Component />} />
