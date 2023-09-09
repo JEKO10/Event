cc6027c97c80e9647d4b72f9f1aeabf7301a9537
Aleksa <aleksa3b@gmail.com>
2023-09-10 00:23:29 +0200

    committees setup

--- src/pages/Committees.tsx
+++ src/pages/Committees.tsx
@@ -5,7 +5,18 @@ const Committees = () => {
   return (
     <>
       <SinglePageNav />
-      <SinglePageHeader title="Odbori" body="a;elas" />
+      <SinglePageHeader
+        title="Odbori"
+        body="Organizacioni Naučni
+      Dr Božo Krstajić, Elektrotehnički fakultet, UCG, Podgorica, MNE - Predsjedavajući konferencije
+Dr Žarko Zečević, Elektrotehnički fakultet, UCG, Podgorica, MNE
+Dr Milutin Radonjić, Elektrotehnički fakultet, UCG, Podgorica, MNE
+Dr Sandra Tinaj, Univerzitet Donja Gorica, Podgorica, MNE
+Dr Tomo Popović, Fakultet za informacione sisteme i tehnologije, UDG, Podgorica, MNE
+Dr Srđan Krčo, DunavNET, Novi Sad, SRB
+Vladan Tabaš, dipl.ing., Čikom, Podgorica, MNE
+      "
+      />
     </>
   );
 };
