aaffa5986d23d50a6aac90585c4aab4bc21364ea
Aleksa <aleksa3b@gmail.com>
2023-09-18 21:23:03 +0200

    tests for header component

---
+++ src/__tests__/components/Header.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "../../components/Header";
+
+describe("Header component", () => {
+  it("should render the header and its elements", () => {
+    render(
+      <Router>
+        <Header />
+      </Router>
+    );
+
+    const navbar = screen.getByRole("navigation");
+
+    expect(navbar).toBeInTheDocument();
+
+    const heroImage = screen.getByAltText(
+      "IZPera Berane Naučna konferencija berane"
+    );
+
+    expect(heroImage).toBeInTheDocument();
+  });
+});
