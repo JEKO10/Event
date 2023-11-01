1dfe93fb4dba88db4b9542a9b4f29f1c9b4665e1
Aleksa Bubanja <aleksa3b@gmail.com>
2023-11-01 16:39:12 +0100

    Agenda page responsive

--- src/assets/style/Agenda.style.tsx
+++ src/assets/style/Agenda.style.tsx
@@ -1,16 +1,38 @@
 import styled from "styled-components";
+import { devices } from "./GlobalStyles";
 
 export const AgendaArticle = styled.article`
   max-width: 85vw;
   margin: 0 auto;
+
+  @media ${devices.laptopS} {
+    max-width: 95vw;
+    padding: 0 20px;
+  }
+
+  @media ${devices.mobile} {
+    padding: 0 10px;
+  }
 
   h2 {
     text-align: center;
     font-weight: 500;
     margin: 5rem 0;
 
+    @media ${devices.tablet} {
+      font-size: 1.1rem;
+    }
+
+    @media ${devices.mobile} {
+      margin: 4rem 0;
+    }
+
     span {
       text-decoration: underline;
+
+      @media ${devices.mobile} {
+        font-size: 1rem;
+      }
     }
   }
 
@@ -18,6 +40,10 @@ export const AgendaArticle = styled.article`
     font-weight: 500;
     text-decoration: underline;
     margin: 5rem 0 1rem;
+
+    @media ${devices.tablet} {
+      margin: 5rem 0 0;
+    }
   }
 
   ul {
@@ -25,6 +51,15 @@ export const AgendaArticle = styled.article`
 
     li {
       margin: 0.5rem 0;
+
+      @media ${devices.tablet} {
+        font-size: 1rem;
+        margin: 1rem 0;
+      }
+
+      @media ${devices.mobile} {
+        margin: 1rem 0 3rem;
+      }
 
       span {
         font-weight: 600;
