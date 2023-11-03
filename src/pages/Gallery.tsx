dcdaff469c50a86e44765d8263d83aa04e8a9b3a
Aleksa Bubanja <aleksa3b@gmail.com>
2023-11-03 23:53:51 +0100

    importing all images from a folder

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import SinglePageHeader from "../components/SinglePageHeader";
 import SinglePageNav from "../components/SinglePageNav";
 
@@ -5,10 +6,28 @@ const Gallery = () => {
   const body = `Foto galerija Naučne Konferencije sa Međunarodnim Učešćem 
         Izazovi i Perspektive za Razvoj Opštine Berane kao Preduslov za Valorizaciju Sjeverne Regije Crne Gore - IZperaBA 2023.`;
 
+  const [galleryImages, setGalleryImages] = useState<string[]>([]);
+
+  useEffect(() => {
+    const gallery = Object.values(
+      import.meta.glob("../assets/slike/*.{png,jpg,jpeg,PNG,JPEG,JPG}", {
+        eager: true,
+        as: "url",
+      })
+    );
+
+    setGalleryImages(gallery);
+  }, []);
+
   return (
     <section>
       <SinglePageNav />
       <SinglePageHeader title="Foto galerija" body={body} />
+      <article>
+        {galleryImages.map((image) => (
+          <img key={image} src={image} alt={`Image`} />
+        ))}
+      </article>
     </section>
   );
 };
